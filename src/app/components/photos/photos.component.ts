a3a9757c5624baf23718a8b5636a7ec018227baa
Rodrigo García Ávila <rodrigogarciaavila26@gmail.com>
2020-12-16 14:37:09 -0600

    Images were collected from firebase and prined on bootstrap cards

--- src/app/components/photos/photos.component.ts
+++ src/app/components/photos/photos.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore, AngularFirestoreCollection } from 'angularfire2/firestore'; 
+import { Observable } from 'rxjs/Observable';
+
+/* Element obtaind ina get petition through an interface */
+export interface Item { name: string; url: string; }
 
 @Component({
   selector: 'app-photos',
@@ -7,7 +12,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PhotosComponent implements OnInit {
 
-  constructor() { }
+  private itemCollection : AngularFirestoreCollection<Item>;
+  items:Observable<Item[]>;
+
+  constructor( private afs: AngularFirestore) { 
+    this.itemCollection = afs.collection<Item>('img'); //where img is the directory you saved images on firebase
+    this.items = this.itemCollection.valueChanges();
+  }
 
   ngOnInit(): void {
   }
